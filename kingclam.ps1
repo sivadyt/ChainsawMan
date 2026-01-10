0939e5b43e59d90349683343d8a6fa3d948804ed
Rugnarokk <55717301+Rugnarokk@users.noreply.github.com>
2026-01-10 15:36:13 -0600

    Add files via upload

---
+++ kingclam.ps1
@@ -0,0 +1,20 @@
+$clamurl = "https://github.com/Cisco-Talos/clamav/releases/download/clamav-1.5.1/clamav-1.5.1.win.x64.msi"
+$giturl = "https://raw.githubusercontent.com/sivadyt/ChainsawMan/refs/heads/main/clamd.conf"
+$giturl2 = "https://raw.githubusercontent.com/sivadyt/ChainsawMan/refs/heads/main/freshclam.conf"
+
+Write-Host "Downloading and installing ClamAV..."
+msiexec /i "https://github.com/Cisco-Talos/clamav/releases/download/clamav-1.5.1/clamav-1.5.1.win.x64.msi" /qn
+Sleep 10
+Write-Host "Done"
+
+Write-Host "Downloading configuration files..."
+Invoke-WebRequest -Uri $giturl -OutFile "C:\Program Files\ClamAV\clamd.conf"
+Invoke-WebRequest -Uri $giturl2 -OutFile "C:\Program Files\ClamAV\freshclam.conf"
+Sleep 5
+Write-Host "Done"
+
+Write-Host "Updating database..."
+Start-Process -FilePath "C:\Program Files\ClamAV\freshclam.exe"
+Write-Host "Done"
+
+Read-Host "Press Enter to finish...."
