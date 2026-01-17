a7ea14da569930713b92cf258041840767303eeb
Rugnarokk <55717301+Rugnarokk@users.noreply.github.com>
2026-01-16 19:03:33 -0600

    Temp

--- DeviceInfo.ps1
+++ DeviceInfo.ps1
@@ -1,11 +1,11 @@
 $ipconfig = ipconfig /all | Select-String -Pattern 'Host Name|Physical Address|IPv4 Address'
-$osVersion = (ver) -replace -replace '$$|$$', ''
-$osVersion = $osVersion.Trim()
+#$osVersion = (ver) -replace -replace '$$|$$', ''
+#$osVersion = $osVersion.Trim()
 
 $device = [ordered]@{
     Name       = ($ipconfig | Where-Object {$_.Line -match 'Host Name'}).Line.Split(':')[1].Trim()
     IPAddress  = ($ipconfig | Where-Object {$_.Line -match 'IPv4 Address'}).Line.Split(':')[1].Trim().Split('(')[0]
     MAC        = ($ipconfig | Where-Object {$_.Line -match 'Physical Address'}).Line.Split(':')[1].Trim()
-    OS Version = $osVersion
+    #OS Version = $osVersion
 }
 $device | Format-List
