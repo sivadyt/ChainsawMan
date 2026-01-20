61b3854749de2d2799ea9de5f95c0ed49c4c6fa1
Rugnarokk <55717301+Rugnarokk@users.noreply.github.com>
2026-01-19 20:33:27 -0600

    Create clamavtask.ps1

---
+++ clamavtask.ps1
@@ -0,0 +1,10 @@
+$action = New-ScheduledTaskAction -Execute "C:\Program Files\ClamAV\clamscan.exe" -Argument "C:\"
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date.AddHours((Get-Date).Hour + 1) -RepetitionInterval (New-TimeSpan -Minutes 60) -RepititionDuration ([TimeSpan]::MaxValue)
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+Register-ScheduledTask `
+-TaskName "ClamAV Hourly Scan" `
+-Action $action `
+-Trigger $trigger `
+-Principal $principal `
+-Description "Runs an hourly scan of ClamAV on the C: drive."
