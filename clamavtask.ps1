e77ec127480fcef04bb280d77ea5c40a297a7975
Rugnarokk <55717301+Rugnarokk@users.noreply.github.com>
2026-01-19 20:42:54 -0600

    Update clamavtask.ps1

--- clamavtask.ps1
+++ clamavtask.ps1
@@ -1,5 +1,5 @@
 $action = New-ScheduledTaskAction -Execute "C:\Program Files\ClamAV\clamscan.exe" -Argument "C:\"
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date.AddHours((Get-Date).Hour + 1) -RepetitionInterval (New-TimeSpan -Minutes 60) -RepititionDuration ([TimeSpan]::MaxValue)
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date.AddHours((Get-Date).Hour + 1) -RepetitionInterval (New-TimeSpan -Minutes 60)
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
 Register-ScheduledTask `
