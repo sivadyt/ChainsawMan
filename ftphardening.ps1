dcb5e3fbfc86e7ae7f4697fc98c05c8a108197d6
Rugnarokk <55717301+Rugnarokk@users.noreply.github.com>
2026-01-19 19:30:13 -0600

    Added wazuh

--- ftphardening.ps1
+++ ftphardening.ps1
@@ -130,6 +130,8 @@ New-NetFirewallRule -DisplayName "CCDC-NTP" -Direction Outbound -Action Allow -P
 
 New-NetFirewallRule -DisplayName "CCDC-ICMP" -Direction Outbound -Action Allow -Protocol ICMPv4 -Profile Any | Out-Null
 
+New-NetFirewallRule -DisplayName "CCDC-Wazuh" -Direction Outbound -Action Allow -Protocol TCP -RemotePort 1514,1515 -Profile Any | Out-Null
+
 New-NetFirewallRule -DisplayName "CCDC-Splunk" -Direction Outbound -Action Allow -Protocol TCP -RemotePort 8089-9997 -Profile Any | Out-Null
 
 # --- Management: lock RDP to admin CIDRs (or disable) ---
